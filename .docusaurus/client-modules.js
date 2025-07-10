7d73255391f96c3862e279fb17f105a18fc2bc1c
beckej13820 <beckej@alumni.lemoyne.edu>
2025-07-10 15:26:08 -0400

    Add github deployment auto

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/beckej/Documents/Github/getting-started-with-eportfolios/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/beckej/Documents/Github/getting-started-with-eportfolios/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/beckej/Documents/Github/getting-started-with-eportfolios/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/beckej/Documents/Github/getting-started-with-eportfolios/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/beckej/Documents/Github/getting-started-with-eportfolios/src/css/custom.css"),
+];
